d754e80d69b2798d856db3427d05da3bdad1b178
fredericpoitevin <frederic.poitevin@gmail.com>
2023-06-11 12:39:08 -0700

    trying to setup the entrypoint so we can run instead of exec, which would allow to set environment variable in the image directly

--- docker/s3df/psconda.sh
+++ docker/s3df/psconda.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source /opt/anaconda/etc/profile.d/conda.sh
+export PATH=/opt/anaconda/bin:${PATH}
 export HDF5_USE_FILE_LOCKING=FALSE
 # needed for SRCF
 export OPENBLAS_NUM_THREADS=1
@@ -8,3 +9,5 @@ export OPENBLAS_NUM_THREADS=1
 export SIT_ROOT=/sdf/group/lcls/ds/ana/
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
 export SIT_PSDM_DATA=/sdf/data/lcls/ds/
+
+exec "$@"
